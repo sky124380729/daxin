d8ecb57d08abeaa11e6322516fda58f4a3475acc
Pink_Bin <124380729@qq.com>
2018-10-08 21:34:32 +0800

    第一次修改

--- js/main.js
+++ js/main.js
@@ -54,6 +54,22 @@ function main() {
       }, 300)
     });
 
+    // 轮播图
+    var mySwiper = new Swiper('.swiper-container', {
+      loop: true, // 循环模式选项
+      speed: 300,
+      autoplay:true,
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
+
   }());
 
 
